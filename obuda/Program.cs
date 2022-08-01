b3bdfcae9297d2771456cb68bb5600e6304c72a5
Bende001 <109483205+Bende001@users.noreply.github.com>
2022-08-01 15:39:50 +0200

    Update Program.cs

--- obuda/Program.cs
+++ obuda/Program.cs
@@ -1,34 +1,112 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace obuda
-{
-    class Program
-    {
+ {
         static void Main(string[] args)
         {
+           
+            //1.Feladat
             Console.WriteLine("1. Feladat");
 
             char[] betuk = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', };
             var halmbetuk = new HashSet<char>(betuk);
+            char[] betuk = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
+            var halmazbetuk = new HashSet<char>(betuk);
 
             Console.Write("Írja be a kívánt szöveget kizárólag az angol abc betűit alkalmazva: ");
             string bekert = Console.ReadLine();
             char[] betuzbekert = bekert.ToCharArray();
+            //szöveg bekérése
+            Console.Write("üzenet: ");
+            string bekertszoveg = "";
+            char[] betuzbekertszoveg = bekertszoveg.ToCharArray();
+            int helyesbetu = 0;
 
             foreach (char betu in betuzbekert)
+                //szöveg vizsgálata
+                while (helyesbetu == 0)
+                {
+                    bekertszoveg = Console.ReadLine();
+                    betuzbekertszoveg = bekertszoveg.ToCharArray();
+
+                    foreach (char betu in betuzbekertszoveg)
+                    {
+                        if (!halmazbetuk.Contains(betu))
+                        {
+                            Console.WriteLine("Kérem csak az angol abc betűit használja. ");
+                            helyesbetu = 0;
+                            break;
+                        }
+                        else helyesbetu++;
+                    }
+                }
+                
+            helyesbetu = 0;
+
+            //kulcs bekérése
+            Console.Write("kulcs: ");
+            string bekertkulcs = "";
+            char[] betuzbekertkulcs = bekertkulcs.ToCharArray();
+
+                //kulcs vizsgálata
+                while (helyesbetu == 0)
+                {
+                    bekertkulcs = Console.ReadLine();
+                    betuzbekertkulcs = bekertkulcs.ToCharArray();
+
+                    foreach (char betu in betuzbekertkulcs)
+                    {
+                        if (!halmazbetuk.Contains(betu))
+                        {
+                            Console.WriteLine("Kérem csak az angol abc betűit használja. ");
+                            helyesbetu = 0;
+                            break;
+                        }
+                        else if (bekertkulcs.Length < bekertszoveg.Length)
+                        {
+                            Console.WriteLine("A szöveggel legalább egyenlő hosszú legyen a kulcs.");
+                            break;
+                        }
+                        else helyesbetu++;
+                    }
+                }
+
+                helyesbetu = 0;
+
+            //rejtjelzett üzenet
+            char[] rejtjelzett = new char[betuzbekertszoveg.Length];
+            char szovegbetu, kulcsbetu;
+            int szovegszam=0, kulcsszam=0;
+
+            //szöveg és kulcs számmá alakítása, rejtjelzett szöveggé alakítása
+            for (int i = 0; i < betuzbekertszoveg.Length; i++)
             {
                 if (!halmbetuk.Contains(betu))
+                for (int j = 0; j < betuk.Length; j++)
+                {
+                    if (betuzbekertszoveg[i] == betuk[j])
+                    {
+                        szovegszam = j;
+                        break;
+                    }
+                }
+                for (int k = 0; k < betuk.Length; k++)
                 {
                     Console.WriteLine("Kérem csak az angol abc betűit használja. ");
                     break;
+                    if (betuzbekertkulcs[i] == betuk[k])
+                    {
+                        kulcsszam = k;
+                        break;
+                    }
                 }
+
+                if ((szovegszam + kulcsszam) > 26) rejtjelzett[i] = betuk[(szovegszam + kulcsszam) % 27];
+
+                else rejtjelzett[i] = betuk[szovegszam + kulcsszam];
             }
+
+            //rejtjelzett szöveg kiírása
+            string rejtjelzettteljes = new string(rejtjelzett);
+            Console.WriteLine("rejtjelzett üzenet: " + rejtjelzettteljes);
+
             Console.ReadKey();
         }
     }
-}
